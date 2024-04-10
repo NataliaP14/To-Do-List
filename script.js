f93a232eb962a6d8c95ce8e9728f2c5cd5a99d11
Natalia Peguero <pegueronatalia@gmail.com>
2024-04-10 00:20:44 -0400

    added main code

---
+++ script.js
@@ -0,0 +1,61 @@
+const input = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
+
+
+const updateCounter = () => {
+    const completedTasks = document.querySelectorAll(".completed").length;
+    const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
+
+    completedCounter.textContent = completedTasks;
+    uncompletedCounter.textContent = uncompletedTasks;
+} 
+
+
+const addTask = () => {
+    const task = input.value.trim();
+    if (!task) {
+        alert("Please input a task");
+        return;
+    }
+
+    const list = document.createElement("li");
+    //adding user input inside the list container
+    list.innerHTML = `
+    <label>
+    <input type = "checkbox">
+    <span>${task}</span>
+    </label>
+    <button class ="edit-button">Edit</button>
+    <button class = "delete-button">Delete</button>
+    `;
+
+    listContainer.appendChild(list);
+    input.value = "";
+
+
+    const checkbox = list.querySelector("input");
+    const editButton = list.querySelector(".edit-button");
+    const editTask = list.querySelector("span");
+    const deleteButton = list.querySelector(".delete-button");
+
+    const checkBoxClick = () => {
+        list.classList.toggle("completed",checkbox.checked);
+        updateCounter();
+    }
+    checkbox.addEventListener("click", checkBoxClick);
+
+
+    const editFunction = () => {
+        const update = prompt("Edit task: ", editTask.textContent);
+        if (update != null) {
+            editTask.textContent = update;
+            list.classList.remove("completed");
+            checkbox.checked = false;
+            updateCounter();
+        }  
+    }
+    editButton.addEventListener("click", editFunction);
+}
+
